fe8246c796fa4df2c1f05b03d9db8f071e3cb354
Matt Lomet <mattlomet@live.com>
2017-05-18 15:58:31 -0400

    first blog table

---
+++ db/migrate/20170518194936_create_blogs_table.rb
@@ -0,0 +1,9 @@
+class CreateBlogsTable < ActiveRecord::Migration[5.1]
+  def change
+    create_table :blogs do |t|
+      t.string :title
+      t.string :content
+      t.integer :user_id
+    end
+  end
+end
