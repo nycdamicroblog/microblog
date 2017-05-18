a64f380c587029ce069787df02443ebad568c567
Ian Zolitor <ianzolitor@gmail.com>
2017-05-18 16:45:53 -0400

    created signup form close #1

--- app.rb
+++ app.rb
@@ -5,3 +5,13 @@ require 'sinatra/activerecord'
 set :database, 'sqlite3:microblog.sqlite3'
 
 require './models'
+
+get '/' do
+
+  erb :index
+end
+
+post  '/' do
+  User.create(username: params[:username],password: params[:password])
+  redirect '/profile'
+end
